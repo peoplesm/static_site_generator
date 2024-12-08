0e912218de4c1f01548c59d77c36b535c4a857ec
peoplesm <mep.summit@gmail.com>
2024-12-08 08:44:54 -0700

    Added test creating nested nodes separately

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import *
+from htmlnode import LeafNode, ParentNode, HTMLNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -166,6 +166,15 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(
             node.to_html(),
             '<p class="introduction"><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>',
+        )
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
         )
 
 
