c44fe1a9d3f11d39ded24990e55b385c28039cf5
peoplesm <mep.summit@gmail.com>
2024-12-08 08:06:52 -0700

    Added ParentNode class and got to_html() to work

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -33,3 +33,21 @@ class LeafNode(HTMLNode):
 
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Parent nodes must have a tag")
+        if self.children is None:
+            raise ValueError("Parent nodes must have children")
+        html_string = ""
+        for node in self.children:
+            html_string += node.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{html_string}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, {self.children}, {self.props})"
--- src/main.py
+++ src/main.py
@@ -2,7 +2,24 @@ from htmlnode import *
 
 
 def main():
-    pass
+    node = ParentNode(
+        "p",
+        [
+            LeafNode("b", "Bold text"),
+            ParentNode(
+                "p",
+                [
+                    LeafNode("i", "nested"),
+                    LeafNode("b", "parent with children"),
+                ],
+            ),
+            LeafNode(None, "Normal text"),
+            LeafNode("i", "italic text"),
+            LeafNode(None, "Normal text"),
+        ],
+    )
+
+    print(node.to_html())
 
 
 if __name__ == "__main__":
