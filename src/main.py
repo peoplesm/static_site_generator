f442ec2db99297c0e324f805e5e4fb3ba6975ee4
peoplesm <mep.summit@gmail.com>
2024-12-18 19:18:30 -0700

    Added regex fxns to extract images and links from markdown

--- src/main.py
+++ src/main.py
@@ -1,10 +1,24 @@
 from htmlnode import *
 from textnode import *
 from split_nodes_delimiter import split_nodes_delimiter
+from extract_markdown import extract_markdown_images, extract_markdown_links
 
 
 def main():
-    print("Hello")
+    print(
+        extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        ),
+        extract_markdown_images(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        ),
+        extract_markdown_images(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        ),
+        extract_markdown_links(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        ),
+    )
 
 
 if __name__ == "__main__":
